6a6ca59b5fe5e8593316dd975feda99ca82ebbda
Vitor Pamplona <vitor@vitorpamplona.com>
2020-11-22 22:00:53 -0500

    Back to two main sites.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,7 +2,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'https://vitorpamplona.com', 'http://localhost:3001'
 
     resource '*',
              headers: :any,
