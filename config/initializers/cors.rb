e4d68d3163ae97fa8ac824cccb8152fe257535d6
Vitor Pamplona <vitor@vitorpamplona.com>
2020-11-22 21:37:59 -0500

    allowing the new staging UI

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,7 +2,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3001'
+    origins 'http://localhost:3001', 'https://vitorpamplona.com/healthpassport-provider-portal-ui/'
 
     resource '*',
              headers: :any,
