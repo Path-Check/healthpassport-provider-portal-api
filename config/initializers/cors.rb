8122ec2338932f5644e703459d42e5ba858918cd
Vitor Pamplona <vitor@vitorpamplona.com>
2020-11-22 21:52:56 -0500

    Accepting everything

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,16 +2,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3001'
-
-    resource '*',
-             headers: :any,
-             methods: %i[get post put patch delete options head],
-             credentials: true
-  end
-
-  allow do
-    origins 'http://vitorpamplona.com'
+    origins '*'
 
     resource '*',
              headers: :any,
