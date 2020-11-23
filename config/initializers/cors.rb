9479d326f564daf60a5ea0acae424b64173669c5
Vitor Pamplona <vitor@vitorpamplona.com>
2020-11-23 10:35:34 -0500

    Moving Staging to heroku

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,7 +2,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'vitorpamplona.com', 'localhost:3001'
+    origins 'healthpassportprovider.herokuapp.com', 'localhost:3001'
 
     resource '*',
              headers: :any,
