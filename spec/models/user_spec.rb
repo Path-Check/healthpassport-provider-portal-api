fe0f343ae38811c1f161e9fa9d80ac7b665ec524
Nic Boie <boie0025@gmail.com>
2020-12-09 11:39:31 -0600

    Use .rspec to define inclusion of rails helper

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,3 @@
-require 'rails_helper'
 RSpec.describe User do
 
   it { is_expected.to validate_presence_of :email }
--- spec/models/vaccination_program_spec.rb
+++ spec/models/vaccination_program_spec.rb
@@ -1,4 +1,3 @@
-require 'rails_helper'
 RSpec.describe VaccinationProgram do
 
   it { is_expected.to belong_to :user }
