5dbae00b5ca2714a364065f208045f3c659a93ba
Vitor Pamplona <vitor@vitorpamplona.com>
2021-03-10 09:46:36 -0500

    Trying to enable CORS for the demo

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,7 +2,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'healthpassport.vitorpamplona.com', 'vitorpamplona.com', 'localhost:3001'
+
     resource '*',
              headers: :any,
              methods: %i[get post put patch delete options head],
